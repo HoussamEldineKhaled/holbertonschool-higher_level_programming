c5d907a23d73d8c419bc66a83003bcd6e66f4ddf
root <houssam.khaled@lau.edu>
2025-03-26 19:57:56 +0000

    first try except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x < 0:
+    if not isinstance(my_list, list):
+        return 0
+    if not isinstance(x, int) or x < 0:
         return 0
     count = 0
     for i in range(0, x):
