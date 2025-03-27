9daaae2bde683f9a80e7df8e5278686ebd9288ee
root <houssam.khaled@lau.edu>
2025-03-27 11:16:48 +0000

    list division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,7 +3,11 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range (0, list_length):
         try:
-            quotiant = my_list_1[i] /my_list_2[i]
+            if my_list_1[i] is None or my_list_2[i] is None:
+                print("None type found")
+                quotiant = 0
+            else:
+                quotiant = my_list_1[i] /my_list_2[i]
         except TypeError:
             print("wrong type")
             quotiant = 0
