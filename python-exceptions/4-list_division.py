f5600b72b5c06146e59cdfaae4bf51a61730c296
root <houssam.khaled@lau.edu>
2025-03-27 11:38:31 +0000

    list division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,11 +7,11 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             quotiant = 0
-        except TypeError:
-            print("wrong type")
-            quotiant = 0
-        except ZeroDivisionError:
-            print("division by 0")
+        except (TypeError, ZeroDivisionError):
+            if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
+                print("division by 0" if my_list_2[i] == 0 else "wrong type")
+            else:
+                print("wrong type")
             quotiant = 0
         finally:
             new_list.append(quotiant)
