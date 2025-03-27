dc0d1affc0aea7c25a5d9231e073468c1b33291a
root <houssam.khaled@lau.edu>
2025-03-27 11:48:45 +0000

    list division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,8 +3,6 @@ list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range (0, list_length):
         try:
-            if not (isinstance(my_list_1[i], (int, float)) or not (isinstance(my_list_2[i], (int, float)):
-                raise TypeError("wrong type")
             quotiant = my_list_1[i] /my_list_2[i]
         except IndexError:
             print("out of range")
