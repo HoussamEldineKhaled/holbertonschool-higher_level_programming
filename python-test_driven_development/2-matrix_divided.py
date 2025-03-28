9b573e4ed0c35e2e31fd8c61262d91cd0e17b01b
root <houssam.khaled@lau.edu>
2025-03-28 08:00:31 +0000

    matrix multiplication

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -19,17 +19,20 @@ def matrix_divided(matrix, div):
     TypeError: many cases
     ZeroDivisionError: if div is zero
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not matrix:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not all(isinstance(ele, (int, float)) for row in matrix for ele in row):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if len(matrix) == 0 or any(len(row) == 0 for row in matrix):
+    if any(len(row) == 0 for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+    if any(len(row) != len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    size = len(matrix[0])
-    if any(len(row) != size for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
     return [[round(el / div, 2) for el in row] for row in matrix]
