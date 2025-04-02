90d7e6efa7cd73c363199bb39d0614684afd0457
root <houssam.khaled@lau.edu>
2025-04-02 17:06:13 +0000

    area

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Square with area"""
+
+
+class Square:
+    """this is the square class"""
+
+    def __init__(self, size=0):
+        """initialise square with area
+
+        Args: 
+           size (int): size of the square
+        """
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """caulculate area        
+        """
+        return self.__size * self.__size
