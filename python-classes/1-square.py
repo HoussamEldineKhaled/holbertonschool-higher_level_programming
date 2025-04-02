67dd68446d4bdf1bd73fbb6ed79b06977c5504d3
root <houssam.khaled@lau.edu>
2025-04-02 14:38:50 +0000

    first problem

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""square class creation"""
+
+
 class Square:
+    """the square it self"""
     def __init__(self, size):
+        """ new square
+
+        Args: size of square
+        """
         self.__size = size
