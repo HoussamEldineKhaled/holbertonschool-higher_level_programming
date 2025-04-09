2037a4b7757befe25e12aa0a4aefd75bc13985ac
root <houssam.khaled@lau.edu>
2025-04-09 16:17:09 +0000

    sorting

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""print a sorted list"""
+
+
+class MyList(list):
+    """
+    print a sorted list
+    """
+    
+    def print_sorted(self):
+        print("{}".format(sorted(self)))
