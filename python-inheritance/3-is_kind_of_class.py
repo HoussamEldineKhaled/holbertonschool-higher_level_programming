cfad31c1d41e6ce34d6c9f37ff46aa5e05ccf4cb
root <houssam.khaled@lau.edu>
2025-04-10 13:57:13 +0000

    inherit check

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""check if both are of same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if one class is an instance of another class"""
+
+    return isinstance(obj, a_class)
