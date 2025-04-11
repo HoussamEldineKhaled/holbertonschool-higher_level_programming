489fe1b7c14474a74ace8e7bff9c8ab2899b8441
root <houssam.khaled@lau.edu>
2025-04-11 17:38:58 +0000

    area and perimeter

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -18,25 +18,52 @@ class Circle(Shape):
     """circle"""
 
     def __init__(self, radius):
+        """
+
+        Args:
+          radius: radius
+        """
         self.radius = radius
 
     def area(self):
+        """
+
+        Return: area
+        """
         return math.pi * self.radius ** 2
 
     def perimeter(self):
+        """
+
+        Return: perimeter
+        """
         return 2 * math.pi * self.radius
 
 class Rectangle(Shape):
     """rectangle"""
     
     def __init__(self, width, height):
+        """
+
+        Args:
+           width: width
+           height: height
+        """
         self.width = width
         self.height = height
 
     def area(self):
+        """
+
+        Return: area
+        """
         return self.width * self.height
 
     def perimeter(self):
+        """
+
+        Return: perimeter
+        """
         return 2 * (self.width + self.height)
 
 def shape_info(shape):
