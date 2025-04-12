a2e0582d7aafcd2c389d091676475133d5bc9cee
root <houssam.khaled@lau.edu>
2025-04-12 20:16:34 +0000

    dragon

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class SwimMixin:
+    def swim(self):
+        print("The creature swims!")
+
+class FlyMixin:
+    def fly(self):
+        print("The creature flies!")
+
+class Dragon(SwimMixin, FlyMixin):
+    print("The dragon roars!")
