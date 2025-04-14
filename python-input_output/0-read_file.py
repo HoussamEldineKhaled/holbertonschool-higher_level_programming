6bbea4d5d143ade623ebb9950a4c4bfb5a48b753
root <houssam.khaled@lau.edu>
2025-04-14 13:34:56 +0000

    reading

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,10 @@
 """"""
 
 def read_file(filename=""):
-    """read text file"""
+    """read text file
+
+    Args:
+       filename (str): name of file
+    """
     with open(filename, encoding="UTF-8") as file:
         print(f.read(), end="")
