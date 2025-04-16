76253d96e0f24637d3711453583c22980e0dd497
root <houssam.khaled@lau.edu>
2025-04-16 19:08:57 +0000

    json

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import json
-"""json string"""
 
 
 def to_json_string(my_obj):
@@ -8,5 +7,8 @@ def to_json_string(my_obj):
 
     Args:
        my_obj (str): input object
+
+    Returns:
+       str: Json string
     """
     return json.dumps(my_obj)
