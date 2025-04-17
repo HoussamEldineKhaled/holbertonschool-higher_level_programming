2f4b5bbc648e52200efa3e154643573d402d40b7
root <houssam.khaled@lau.edu>
2025-04-17 12:43:35 +0000

    tbh

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-""""""
+"""save to a json file"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """method for saving
+
+    Args:
+       my_obj: input object
+       filename: name of file
+    Return:
+"""
+    with open(filename)
