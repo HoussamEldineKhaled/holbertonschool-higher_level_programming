063de92436996c43c842d9b46feab1742d6d3217
root <houssam.khaled@lau.edu>
2025-04-18 10:44:51 +0000

    load and save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""add new item"""
 
-list = []
+import sys
+from os.path import exists
 
+saving = __import__('5-save_to_json_file').save_to_json_file
+loading = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    items = loading(filename)
+except FileNotFoundError:
+    items = []
+items.extend(sys.argv[1:])
+
+saving(items, filename)
