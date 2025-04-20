aa5ae4be2bc37b4c26abac1450ba318963188136
root <houssam.khaled@lau.edu>
2025-04-20 17:17:13 +0000

    class

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""custom classes"""
+
+
+import pickle
+
+
+class CustomObject:
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def serlialize(self, filename):
+
+        try:
+            with open(filename, "wb") as file:
+                pickle.dump(self, filename)
+        except (pickle.PickleError, IOError, Exception) as e:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as file:
+                return pickle.load(cls)
+        except (pickle.PickleError, IOError, Exception) as e:
+            return None
