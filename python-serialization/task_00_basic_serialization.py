bce0a7e68e3e9771a2447d69476656a6ebea44ba
root <houssam.khaled@lau.edu>
2025-04-20 15:11:56 +0000

    basic serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,9 +6,22 @@ import pickle
 
 
 def serialize_and_save_to_file(data, filename):
+    """save to file
+
+    Args:
+       data: data
+       filename: name of file
+"""
     with open(filename, "wb") as file:
         pickle.dump(data, file)
 
 def load_and_deserialize(filename):
+    """deserialize file
+
+    Args:
+       filename: name of file
+
+    Returns: the loaded pickle
+"""
     with open(filename, "rb") as file:
         return pickle.load(file)
