f28a18350c199084f79b304cf87390fe56d351be
root <houssam.khaled@lau.edu>
2025-04-24 10:04:17 +0000

    copy list

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""function to copy a list"""
 
 
 def copy_list(a_list):
-    return a_list
+    """copy list
+
+    Args:
+       a_list: list input
+
+    Returns: copied list
+    """
+    return a_list[:]
