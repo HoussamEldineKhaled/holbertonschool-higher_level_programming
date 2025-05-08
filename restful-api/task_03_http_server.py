baeb3424d071b84258decc07c7f1f2560de230c8
root <houssam.khaled@lau.edu>
2025-05-08 16:05:12 +0000

    to be continued

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write("Hello, this is a simple API!".encode('utf-8'))
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write("Hello, this is a simple API!".encode('utf-8'))
+        elif self.path == '/data':
+            dataset = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(dataset).encode('utf-8'))
+        elif self.path == '/status':
+            self.send_response(200)
+            
 
 
 if __name__ == '__main__':
