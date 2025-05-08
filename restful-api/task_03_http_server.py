093adf23a5f39ae080650a64a881f6311d3c140d
root <houssam.khaled@lau.edu>
2025-05-08 18:13:35 +0000

    get

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -23,10 +23,10 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write("OK".encode('utf-8'))
         else:
-            self.send_response(404, "Not Found")
+            self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"404 Not Found\n")
+            self.wfile.write(b"404")
 
 
 if __name__ == '__main__':
