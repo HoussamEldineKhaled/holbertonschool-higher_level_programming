c9e52da6a2d1d55cb056d9ca0e9bc7de03b05b08
root <houssam.khaled@lau.edu>
2025-05-08 15:25:39 +0000

    to be continued

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -6,9 +6,11 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b'Hello, this is a simple API!')
+        self.wfile.write("Hello, this is a simple API!".encode('utf-8'))
 
 
-server = HTTPServer(('', 8000), HTTPRequestHandler)
-server.serve_forever()
+if __name__ == '__main__':
+    httpd = HTTPServer(('', 8000), HTTPRequestHandler)
+    httpd.serve_forever()
