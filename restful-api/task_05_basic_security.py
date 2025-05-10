337f004a093fa22cf10d9832e048b7cf5104e73c
root <houssam.khaled@lau.edu>
2025-05-10 10:52:36 +0000

    password auth

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+users =  {
+    "barry": generate_password_hash("all8"),
+    "nagham": generate_password_hash("jfd")
+}
 
