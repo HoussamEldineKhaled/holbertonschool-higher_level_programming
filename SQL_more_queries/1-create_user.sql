e0afee055aa333838bdd34bfca2412a38d9a2989
root <houssam.khaled@lau.edu>
2025-05-17 22:30:59 +0000

    lol

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- root user
 CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES O *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
