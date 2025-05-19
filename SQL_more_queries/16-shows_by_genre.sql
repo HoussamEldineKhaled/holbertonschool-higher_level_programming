f652b4214f65b9ed1fe94a19eb15fd1568a91f35
root <houssam.khaled@lau.edu>
2025-05-19 22:20:38 +0000

    lol

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -4,5 +4,5 @@ FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
-ON tv_shows_genres.genre_id = tv_genres.id
+ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
