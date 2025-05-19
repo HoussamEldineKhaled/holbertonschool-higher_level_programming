7a850249c1d00b9dc1eb5a3186ec3160839f7472
root <houssam.khaled@lau.edu>
2025-05-19 18:19:04 +0000

    lol

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,5 +2,8 @@
 SELECT tv_shows.title, NULL AS genre_id
 FROM tv_shows
 WHERE NOT EXISTS (
-
+SELECT 1
+FROM tv_show_genres
+WHERE tv_show_genres.show_id = tv_shows.id
 )
+ORDER BY tv_shows.title ASC;
